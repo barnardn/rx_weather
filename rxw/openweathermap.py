1402ee5d55a4158d0faf76b0b798bc778b839d98
Norm Barnard <barnardn@gmail.com>
2018-02-12 16:01:07 -0500

    refactored display observable

--- rxw/openweathermap.py
+++ rxw/openweathermap.py
@@ -30,7 +30,7 @@ class CurrentConditions:
     def __init__(self, api_key: str):
         self.api_key = api_key
 
-    def fetch(self, zip: str, temp_only: bool=False):
+    def show_for(self, zip: str, temp_only: bool=False):
         self.rx_fetch(zip) \
                 .flat_map(lambda js, _: self.parse_weather(js)) \
                 .subscribe(
@@ -63,53 +63,65 @@ class CurrentConditions:
         """ 
         extract the various weather readings from the json
         blob and return a WeatherForecast object 
-        """
-        if len(json) == 0:
-            return rx.Observable.throw(Exception('No Weather Data'))
+        """        
+        def observable(observer):
+            try:
+                if len(json) == 0:
+                    raise(Exception('No Weather Data'))
+                
+                location = Location(id=json['id'])
+                location.name = json['name']
+                
+                if 'sys' in json:
+                    sys = json['sys']
+                    location.country = sys['country']
+                    sunrise = SolarTimes.utc_to_localdatetime(sys['sunrise'])
+                    sunset = SolarTimes.utc_to_localdatetime(sys['sunset'])
+                    location.solar = SolarTimes(sunrise, sunset)
+                else:
+                    raise Exception("Weather data invalid, missing 'sys'")
+                
+                weather = WeatherForecast(location)        
+                if 'coord' in json:
+                    lat = json['coord']['lat']
+                    lon = json['coord']['lon']
+                    weather.location.geo_location = GeoPoint(lat, lon)
+                if 'main' not in json:
+                    raise Exception("Weather data invalid, missing 'main'")
+                    
+                main = json['main']
+                cc = ClimateCondition()
+                cc.temperature = Measurement(
+                    main['temp'], 
+                    default_unit('temp'))
+                cc.humidity = Measurement(
+                    main['humidity'], 
+                    default_unit('humidity'))
+                if 'wind' in json:
+                    wind = json['wind']
+                    speed = Measurement(wind['speed'], default_unit('speed'))
+                    dir = Measurement(wind['deg'], default_unit('deg'))
+                    cc.wind = Vector(speed, dir)
             
-        location = Location(id=json['id'])
-        location.name = json['name']
-
-        if 'sys' in json:
-            sys = json['sys']
-            location.country = sys['country']
-            sunrise = SolarTimes.utc_to_localdatetime(sys['sunrise'])
-            sunset = SolarTimes.utc_to_localdatetime(sys['sunset'])
-            pdb.set_trace()
-            location.solar = SolarTimes(sunrise, sunset)
-        else:
-            return rx.Observable.throw(Exception('No Weather Data'))
-            
-        weather = WeatherForecast(location)        
-        
-        if 'coord' in json:
-            lat = json['coord']['lat']
-            lon = json['coord']['lon']
-            weather.location.geo_location = GeoPoint(lat, lon)
-        if 'main' not in json:
-            return rx.Observable.just(weather)
-            
-        main = json['main']
-        cc = ClimateCondition()
-        cc.temperature = Measurement(main['temp'], default_unit('temp'))
-        cc.humidity = Measurement(main['humidity'], default_unit('humidity'))
-        if 'wind' in json:
-            wind = json['wind']
-            speed = Measurement(wind['speed'], default_unit('speed'))
-            dir = Measurement(wind['deg'], default_unit('deg'))
-            cc.wind = Vector(speed, dir)
-            
-        if 'weather' in json:
-            ps = json['weather']
-            params = [Parameter(p['main'], p['description']) for p in ps]
-            cc.conditions = params
-            
-        weather.current = cc
-        return rx.Observable.just(weather)
-
+                if 'weather' in json:
+                    ps = json['weather']
+                    params = [Parameter(p['main'], p['description']) 
+                                for p in ps]
+                    cc.conditions = params                                        
+                    
+                weather.current = cc
+                
+            except Exception as e:
+                observer.on_error(e)
+            else:
+                observer.on_next(weather) 
+            finally:
+                return lambda: None
+        return rx.Observable.create(observable)
+                
     def _handle_error(self, e: Exception): 
         """ display an error message """
-        if e is requests.HTTPError:
+        if type(e) is requests.HTTPError:
             if e.response.status_code == 404:
                 print("Unable to find weather at the location specified")
             else:
